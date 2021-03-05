7e17be0cc9c11c4828e6d62bac0b77eec35c51c5
Hiển Bùi <buihien0005@gmail.com>
2021-03-05 07:48:09 +0700

    Create service category get all

--- eCommerce.Application/Catalog/Categories/CategoryService.cs
+++ eCommerce.Application/Catalog/Categories/CategoryService.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using eCommerce.Data.EF;
+using eCommerce.Model.Catalog.Categories;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerce.Application.Catalog.Categories
 {
-    public interface CategoryService
+    public class CategoryService : ICategoryService
     {
-
+        private readonly eComContext _context;
+        public CategoryService(eComContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<CategoryView>> CategoryGetAll(string languageId)
+        {
+            var query = from c in _context.Categories
+                        join ct in _context.CategoryTranslations on c.Id equals ct.CategoryId
+                        select new { c, ct };
+            return await query.Select(x => new CategoryView()
+            {
+                Id = x.c.Id,
+                ParentId = x.c.ParentId,
+                Name = x.ct.Name
+            }).ToListAsync();
+        }
     }
 }
---
+++ eCommerce.Model/Catalog/Categories/CategoryView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Model.Catalog.Categories
+{
+    public class CategoryView
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
